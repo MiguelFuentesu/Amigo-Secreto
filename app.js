b6ab91264cce54b3f788ecfaddd0717858ea2d6b
MiguelFuentesu <xxmiguel266@gmail.com>
2025-02-12 01:16:21 -0300

    Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+let nombreAmigo = document.getElementById('amigo').value.trim();
+if (nombreAmigo !== "" && nombreAmigo !== "Escribe un nombre"){
+    amigos.push(nombreAmigo);
+    document.getElementById('amigo').value = "";
+    console.log("Se agrega al listado amigos:"+ nombreAmigo);
+}
+else{
+    alert("Por favor, inserte un nombre.");
+}
+}
