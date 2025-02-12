4b251021c845f6e05befe13b3c1e99beaef8eefb
MiguelFuentesu <xxmiguel266@gmail.com>
2025-02-12 01:59:55 -0300

    Funcion 1.2
    Se añade funciones y comentarios para cada función creada.
    Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
    
    Tareas específicas:
    
    Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
    
    Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
    
    Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
    
    Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 //Se genera variable array para almacenar los datos de amigos
 let amigos = [];
+
 //Funcion que valida el elemento HTML 'amigo'.
 function agregarAmigo(){
+//capturar el valor de amigo y un .trim para eliminar espacios
 let nombreAmigo = document.getElementById('amigo').value.trim();
+//Validar que el valor sea distinto que nada y aparte distinto al placeholder, de lo contrario ingresa ese dato.
 if (nombreAmigo !== "" && nombreAmigo !== "Escribe un nombre"){
+    //añadir a la lista amigos
     amigos.push(nombreAmigo);
+    //limpiar input amigo
     document.getElementById('amigo').value = "";
+    //log temporal (comentar al terminar el codigo)
     console.log("Se agrega al listado amigos:"+ nombreAmigo);
+    //Se añade funcion para mostrar los amigos agregados
+    añadirElementosAmigos();
 }
 else{
     alert("Por favor, inserte un nombre.");
+}
+}
+
+//Funcion para recorrer la lista amigos
+function añadirElementosAmigos(){
+//obtener listaAmigos
+let listaAmigos = document.getElementById('listaAmigos');
+
+//Limpiar lista amigos
+listaAmigos.innerHTML = "";
+
+//Bucle para añadir elementos <li>
+for (let i = 0; i < amigos.length; i++){
+    //Se crea elemento li
+    let li = document.createElement('li');
+    //Selecciona segun la variable i la posicion del array para añadir el texto al elemento li
+    li.textContent = amigos[i];
+    // Se añade a la lista total actualizada los elementos li
+    listaAmigos.appendChild(li);
 }
 }
